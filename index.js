cc0b04c9e13c2360c759725c6f4b49660199973d
Julianna Tetreault <juliannatetreault@icloud.com>
2018-08-09 07:22:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+getFirstSelector(selector); {
+  return document.querySelector(selector);
+}
 
+nestedTarget(); {
+  return document.querySelector('#nested .target');
+}
+
+increaseRankBy(n); {
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++);
+  {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+deepestChild(); {
+return document.querySelector('#grand-node div div div div');
+}
